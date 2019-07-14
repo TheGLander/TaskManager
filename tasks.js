324afc1d2fc4a3d5c830237ead2ae6977bf4252d
Lubomir <lubomir.yudenko@gmail.com>
2019-07-14 17:22:41 +0300

    Added Import/Export

--- tasks.js
+++ tasks.js
@@ -8,7 +8,7 @@ function RefreshTasks() {
     document.getElementById("tasks").innerHTML = ""
     for (var i = 0; i != tasks.length; i++) {
         document.getElementById("tasks").innerHTML += "<li>" + tasks[i] +
-            '<input type="button" value="Done" onclick="RemoveTask(' + i + ')" >\n'
+            '<input type="button" id="my_centered_buttons" value="Done" onclick="RemoveTask(' + i + ')" >\n'
     }
     localStorage.setItem("Tasks", tasks);
 }
@@ -35,8 +35,29 @@ function ResetTasks() {
 }
 
 function ImportTasks() {
-    tasks = ["Call mum", "Dance"]
+    try {
+        if (JSON.parse(Base64.decode(document.getElementById("Code").value)).some(i => typeof i !== "string")) {
+            throw "Not only strings in the array OR not an array.";
+        }
+    } catch (err) {
+        alert("Invalid code!");
+        console.error(err)
+        return "Evil";
+    }
+    tasks = JSON.parse(Base64.decode(document.getElementById("Code").value))
     RefreshTasks()
+}
+function ExportTasks() {
+    try {
+        if (tasks.some(i => typeof i !== "string")) {
+            throw "Not only strings in the array OR not an array.";
+        }
+    } catch (err) {
+        alert("Data corrupted, cannot export.");
+        console.error(err)
+        return "Corrupted.";
+    }
+    document.getElementById("Code").value = Base64.encode(JSON.stringify(tasks))
 }
 inputId = document.getElementById('TaskName');
 inputId.addEventListener('keyup', function onEvent(e) {
